6d36c3a1c1444ff656ae200cccccdf7fbf91720e
xy2511040 <806143469@qq.com>
2018-07-08 12:48:36 +0800

    首次提交项目框架搭建

---
+++ src/cn/jsmoon/util/User.java
@@ -0,0 +1,77 @@
+package cn.jsmoon.util;
+
+public class User {
+	
+	private Integer id;	//ID
+	
+	private String userName;	//用户名
+	
+	private String password;	//密码
+	
+	private String trueName;	//真实姓名
+	
+	private String roleName;	//角色
+	
+	private Integer deptId;	//部门ID
+	
+	private Integer deptName;	//部门名字
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getTrueName() {
+		return trueName;
+	}
+
+	public void setTrueName(String trueName) {
+		this.trueName = trueName;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public Integer getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(Integer deptId) {
+		this.deptId = deptId;
+	}
+
+	public Integer getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(Integer deptName) {
+		this.deptName = deptName;
+	}
+
+
+	
+}
